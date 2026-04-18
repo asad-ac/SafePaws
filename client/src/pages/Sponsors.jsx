9bdfa54b9ee26b8d125a81ade09fd3d86f238874
asad-ac <asadachau@gmail.com>
2026-04-18 10:52:01 -0400

    mapped over sponsors

--- client/src/pages/Sponsors.jsx
+++ client/src/pages/Sponsors.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
-const sponsors = () => {
+const Sponsors = () => {
 
-    const [sponsors, setSponsors] = React.useState()
+    const [sponsors, setSponsors] = useState([])
+
+    useEffect(() => {
+        const fetchAllSponsors = async () => {
+            const response = await fetch (`http://localhost:3001/sponsors`)
+            const data = await response.json()
+            setSponsors(data)
+        }
+        fetchAllSponsors()
+    },[])
     
   return (
     <div>
-      
+      {/* we map sponsors state with all fields of name, amount, address, phone, email */}
+      {sponsors.length > 0 ? sponsors.map((sponsor) => {
+        return (
+            <div key={sponsor.sponsor_id}> 
+                <h1> {sponsor.name} </h1> 
+                <h1> {sponsor.amount} </h1>
+                <h1> {sponsor.address} </h1>
+                <h1> {sponsor.phone} </h1>
+                <h1> {sponsor.email} </h1>
+            </div>
+    )
+      }) : <h1> No sponsors added </h1>}
     </div>
   )
 }
 
-export default sponsors
+export default Sponsors
