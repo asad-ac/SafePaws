d758ad653967db407510e35ecd0b019a845a6c17
asad-ac <asadachau@gmail.com>
2026-04-18 11:49:20 -0400

    feat: passing props of boolean and data to addsponsor modal

--- client/src/components/AddSponsor.jsx
+++ client/src/components/AddSponsor.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import {useState} from 'react'
 
-const AddSponsor = () => {
+const AddSponsor = (props) => {
+
+    const [addSponsor, setAddSponsor] = useState([])
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify(addSponsor)
+    }
+    
+    const add = async () => {
+        const insert = await fetch('http://localhost:3001/sponsors', options)
+    }   
+
   return (
     <div>
-      
+      <form onSubmit={add}>
+        <label> Name: </label>
+        <input required type="text" />
+        <label> Amount: </label>
+        <input required type="text" />
+        <label> Address: </label>
+        <input required type="text" />
+        <label>Phone: </label>
+        <input required type="number" />
+        <label>Email: </label>
+        <input required type="email" />
+      </form>
+      <button onClick={add}> Submit </button>
+        <button> Cancel </button>
     </div>
   )
 }
--- client/src/pages/Sponsors.jsx
+++ client/src/pages/Sponsors.jsx
@@ -1,8 +1,10 @@
 import {useState, useEffect} from 'react'
+import AddSponsor from '../components/AddSponsor'
 
 const Sponsors = () => {
 
     const [sponsors, setSponsors] = useState([])
+    const [isAddOpen, setIsAddOpen] = useState(false)
 
     useEffect(() => {
         const fetchAllSponsors = async () => {
@@ -17,10 +19,11 @@ const Sponsors = () => {
     <div>
     <h1> Sponsors </h1>
       {/* we map sponsors state with all fields of name, amount, address, phone, email */}
+      <button onClick={() => setIsAddOpen(true)}>Add Sponsor</button>
       {sponsors.length > 0 ? sponsors.map((sponsor) => {
         return (
-            <div className=''>
-                <div className='' key={sponsor.sponsor_id}> 
+            <div key={sponsor.sponsor_id} className=''>
+                <div className=''> 
                     <p> {sponsor.name} </p> 
                     <p> ${sponsor.amount} / Monthly </p>
                     <p> {sponsor.address} </p>
@@ -32,6 +35,12 @@ const Sponsors = () => {
             </div>
         )
       }) : <h2> No sponsors added </h2>}
+
+        {isAddOpen && (
+            <AddSponsor
+            setIsAddOpen={setIsAddOpen}
+            setSponsors={setSponsors} />
+        )}
     </div>
   )
 }
