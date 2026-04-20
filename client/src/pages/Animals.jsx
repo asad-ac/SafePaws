eef3256636fac07bc9d14b720337e799781c31d7
asad-ac <asadachau@gmail.com>
2026-04-20 18:02:03 -0400

    fix: random bugs

--- client/src/pages/Animals.jsx
+++ client/src/pages/Animals.jsx
@@ -18,17 +18,18 @@ const Animals = () => {
         <div>
             {animals.length > 0 ? animals.map((animal) => {
                 return (
-
-                <div key={animal.animal_id} style={{backgroundImage: `${animal.image_url}`}}>
-                    <h1> {animal.name} </h1>
-                    <p> {animal.species} </p>
-                    <p> {animal.weight} </p>
-                    {animal.tags.length > 0 ? animal.tags.map((tag) => {
-                        <div key={tag[tag_id]}>
-                            <p> {tag.name} </p>
-                        </div>
-                    }): null}
-                </div>
+                    <div key={animal.animal_id} style={{backgroundImage: `url(${animal.image_url})`}}>
+                        <h1> {animal.name} </h1>
+                        <p> {animal.species} </p>
+                        <p> {animal.weight} </p>
+                        {animal.tags.length > 0 ? animal.tags.map((tag) => {
+                            return (
+                                <div key={tag.tag_id}>
+                                    <p> {tag.name} </p>
+                                </div>
+                            )
+                        }): null}
+                    </div>
                 )
                 }): <h1>No animals added </h1>}
         </div>
