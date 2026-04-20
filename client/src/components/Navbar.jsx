11c378af30d150f7fef4402c295cc16cc34f4b05
asad-ac <asadachau@gmail.com>
2026-04-19 22:33:56 -0400

    feat ig lol quickly added react router dom links without install tho

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const NavBar = () =>{
     return (
@@ -6,18 +7,10 @@ const NavBar = () =>{
             <div className='SafePaws-Logo'>
                 <img href=''></img>
             </div>
-            <ul>
-                Animals
-            </ul>
-            <ul>
-                Sponsors
-            </ul>
-            <ul>
-                Volunteers
-            </ul>
-            <ul>
-                Settings
-            </ul>
+            <Link> Animals </Link>
+            <Link> Sponsors </Link>
+            <Link> Volunteers </Link>
+            <Link> Settings </Link>
             {/* profile icon could also be a component? */}
             <div className='Profile-Icon'>
                 <img href=''></img>
