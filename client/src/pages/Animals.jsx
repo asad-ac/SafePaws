2d6c84fb4233c1bd941f78f6282c9b9ab6b2ad42
asad-ac <asadachau@gmail.com>
2026-04-20 17:54:14 -0400

    feat: mapping tags

--- client/src/pages/Animals.jsx
+++ client/src/pages/Animals.jsx
@@ -17,8 +17,13 @@ const Animals = () => {
     <>
         <div>
             {animals.length > 0 ? animals.map((animal) => {
-                <div style={{backgroundImage: `${animal.im}`}}>
-                    <h1>{animal.name}</h1>
+                <div style={{backgroundImage: `${animal.image_url}`}}>
+                    <h1> {animal.name} </h1>
+                    <p> {animal.species} </p>
+                    <p> {animal.weight} </p>
+                    {animal.tags.length > 0 ? animal.tags.map((tag) => {
+                        <p> {tag} </p>
+                    }): null}
                 </div>
                 }): <h1>No animals added </h1>}
         </div>
