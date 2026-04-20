799573055aee4e2b471d60ab591f5c2b83f6f577
asad-ac <asadachau@gmail.com>
2026-04-20 17:59:19 -0400

    feat: forgot keys

--- client/src/pages/Animals.jsx
+++ client/src/pages/Animals.jsx
@@ -17,14 +17,19 @@ const Animals = () => {
     <>
         <div>
             {animals.length > 0 ? animals.map((animal) => {
-                <div style={{backgroundImage: `${animal.image_url}`}}>
+                return (
+
+                <div key={animal.animal_id} style={{backgroundImage: `${animal.image_url}`}}>
                     <h1> {animal.name} </h1>
                     <p> {animal.species} </p>
                     <p> {animal.weight} </p>
                     {animal.tags.length > 0 ? animal.tags.map((tag) => {
-                        <p> {tag} </p>
+                        <div key={tag[tag_id]}>
+                            <p> {tag.name} </p>
+                        </div>
                     }): null}
                 </div>
+                )
                 }): <h1>No animals added </h1>}
         </div>
     </>
