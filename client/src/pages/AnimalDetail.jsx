d925bbb6ec642ef021549c8fda1c7fb6683eeb1c
asad-ac <asadachau@gmail.com>
2026-04-22 12:46:41 -0400

    fix: bugs and setting up add animal

--- client/src/pages/AnimalDetail.jsx
+++ client/src/pages/AnimalDetail.jsx
@@ -29,7 +29,7 @@ const AnimalDetail = () => {
                         <img src={animal.image_url} alt={`${animal.name} the ${animal.species}`} />
                         <h2> <strong> Name </strong> {animal.name} </h2>
                         <h2> <strong> Species </strong> {animal.species} </h2>
-                        <h2> <strong> Age </strong> {animal.age} </h2>
+                        <h2> <strong> Age </strong> {animal.age} years old </h2>
                         <h2> <strong> Weight (pounds) </strong> {animal.weight} lbs </h2>
                         <h2> <strong> Height (feet) </strong> {animal.height} ft </h2>
                         <h2> <strong> Intake </strong> {" "}{animal.date_intake && new Date(animal.date_intake).toLocaleDateString('en-US', {month: 'short', day: 'numeric', year: 'numeric'})} </h2>
@@ -45,6 +45,7 @@ const AnimalDetail = () => {
                             <button> <FaRegTrashAlt /> Delete </button>
                         </div>
 
+                        <h3> Tags </h3>
                         {animal.tags && animal.tags.length > 0 && animal.tags.map((tag) => (
                         <div key={tag.id}>
                             <p>{tag.name}</p>
