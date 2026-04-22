717e4a4c02db6d114e833d873c1b18d27f3fb684
asad-ac <asadachau@gmail.com>
2026-04-22 15:26:27 -0400

    feat: handle select options changes

--- client/src/components/AddAnimal.jsx
+++ client/src/components/AddAnimal.jsx
@@ -2,7 +2,12 @@ import {useState} from 'react'
 
 const AddAnimal = () => {
 
-    const [form, setForm] = useState({name: '', description: '', age: '', weight: '', height: '', image_url: '', date_intake: '', species: '', cleaning_status: '', care_status: '', feeding_status: '', sanctuary_id: 1})
+    // TODO: selected tags
+    // TODO: fix handlesubmit with spread
+    // TODO: cleaning status, care status, feeding status
+    // TODO: pass props?
+
+    const [form, setForm] = useState({name: '', description: '', age: '', weight: '', height: '', image_url: '', date_intake: '', species: '', cleaning_status: false, care_status: false, feeding_status: false, sanctuary_id: 1})
     const [selectedTags, setSelectedTags] = useState([])
 
     const handleChange = (e) => {
@@ -13,7 +18,17 @@ const AddAnimal = () => {
         }))
     }
 
-    const handleSubmit = async () => {
+    const handleSelectChange = (e) => {
+        const { name, value } = e.target
+      
+        setForm((prev) => ({
+          ...prev,
+          [name]: value === "true"
+        }))
+      }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
         const options = {
             method: 'POST',
             headers: {
@@ -32,18 +47,32 @@ const AddAnimal = () => {
       <form onSubmit={handleSubmit}>
         <label> Name </label>
         <input required type='text' name='name' value={form.name} onChange={handleChange} />
-        <label> Age </label>
-        <input required type="text" name='name' value={form.name} onChange={handleChange} />
-        <label> Weight  </label>
-        <input required type="number" name='amount' value={form.amount} onChange={handleChange} />
-        <label> Height </label>
-        <input required type="text" name='address' value={form.address} onChange={handleChange} />
-        <label> Image Link  </label>
-        <input required type="tel" name='phone' value={form.phone} onChange={handleChange} />
-        <label> Date Intake  </label>
-        <input required type="email" name='email' value={form.email} onChange={handleChange} />
         <label> Description </label>
-        <textarea>  </textarea>
+        <textarea name='description' value={form.description} onChange={handleChange}></textarea>
+        <label> Age </label>
+        <input required type="text" name='age' value={form.age} onChange={handleChange} />
+        <label> Weight  </label>
+        <input required type="number" name='weight' value={form.weight} onChange={handleChange} />
+        <label> Height </label>
+        <input required type="text" name='height' value={form.height} onChange={handleChange} />
+        <label> Image Link  </label>
+        <input required type="text" name='image_url' value={form.image_url} onChange={handleChange} />
+        <label> Date Intake  </label>
+        <input required type='date' name='date_intake' value={form.date_intake} onChange={handleChange} />
+        <label> Species </label>
+        <input required type='text' name='species' value={form.species} onChange={handleChange} />
+        <select name='feeding_status' value={form.feeding_status} onChange={handleSelectChange}>
+            <option value="false"> Pending </option>
+            <option value="true"> Complete </option>
+        </select>
+        <select name='cleaning_status' value={form.cleaning_status} onChange={handleSelectChange}>
+            <option value="false"> Pending </option>
+            <option value="true"> Complete </option>
+        </select>
+        <select name='care_status' value={form.care_status} onChange={handleSelectChange}>
+            <option value="false"> Pending </option>
+            <option value="true"> Complete </option>
+        </select>
       </form>
     </div>
   )
