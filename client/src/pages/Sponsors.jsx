f1b240e8f29fb4e329fbf23603bf7cbdf804c777
asad-ac <asadachau@gmail.com>
2026-04-25 12:49:23 -0400

    feat: search by name on sponsors

--- client/src/pages/Sponsors.jsx
+++ client/src/pages/Sponsors.jsx
@@ -13,6 +13,7 @@ const Sponsors = () => {
     const [isAddOpen, setIsAddOpen] = useState(false)
     const [isEditOpen, setIsEditOpen] = useState(false)
     const [selected, setSelected] = useState(null)
+    const [search, setSearch] = useState('')
 
     useEffect(() => {
         const fetchAllSponsors = async () => {
@@ -39,14 +40,19 @@ const Sponsors = () => {
 
         return data
     }
+
+    const searchSponsors = sponsors.filter((s) => {
+        return s.name.toLowerCase().includes(search.trim().toLowerCase())
+    })
     
   return (
     <>
         <div>
             <h1> Sponsors </h1>
+            <input type='search' value={search} placeholder='Search by name' onChange={(e) => setSearch(e.target.value)} />
             {/* we map sponsors state with all fields of name, amount, address, phone, email */}
             <button onClick={() => setIsAddOpen(true)}> <IoAddSharp /> Add Sponsor </button>
-            {sponsors.length > 0 ? sponsors.map((sponsor) => {
+            {searchSponsors.length > 0 ? searchSponsors.map((sponsor) => {
                 return (
                     <div key={sponsor.sponsor_id} className=''>
                         <div className=''> 
