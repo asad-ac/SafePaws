2b501219766e28106b5207dde0333dbc18e6e888
asad-ac <asadachau@gmail.com>
2026-04-28 11:52:09 -0400

    feat: skeleton animals page component

---
+++ client/src/components/SkeletonAnimals.jsx
@@ -0,0 +1,12 @@
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+const SkeletonAnimals = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SkeletonAnimals
