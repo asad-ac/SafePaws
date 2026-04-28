fd42b2b57a058059f9f79557cab77211c4b8276e
asad-ac <asadachau@gmail.com>
2026-04-28 19:14:01 -0400

    feat: all delete modals close on outside click

--- client/src/pages/Sponsors.jsx
+++ client/src/pages/Sponsors.jsx
@@ -165,7 +165,7 @@ const Sponsors = () => {
                                 <button command="show-modal" commandfor="delete-confirmation" className="btn-delete"> <FaRegTrashAlt /> Delete </button>
                             </div>
 
-                            <dialog id="delete-confirmation" className="delete-dialog">Are you sure you'd like to delete the sponsor: {sponsor.name}? This action can NOT be undone. 
+                            <dialog id="delete-confirmation" onClick={closeDialogOutside} className="delete-dialog">Are you sure you'd like to delete the sponsor: {sponsor.name}? This action can NOT be undone. 
                                     <button commandfor="delete-confirmation" command="close" className="btn-cancel">Close</button>
                                     <button onClick={() => deleteSponsor(sponsor)} className="btn-delete"> DELETE </button>
                             </dialog>
