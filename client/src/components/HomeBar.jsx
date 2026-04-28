bbd775a551a5142b5b2a30633eb5553f0aead760
asad-ac <asadachau@gmail.com>
2026-04-28 17:23:43 -0400

    feat: added navlink function

--- client/src/components/HomeBar.jsx
+++ client/src/components/HomeBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import '../css/HomeBar.css'
 import logo from '../assets/logo.svg'
   
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import {NavLink, NavLink} from 'react-router-dom'
 import '../css/NavBar.css'
 
 const NavBar = () =>{
     return (
         <nav>
             <div className="navbar-links">
-                <Link to='/animals'> Animals </Link>
-                <Link to='/sponsors'> Sponsors </Link>
-                <Link to='/volunteers'> Volunteers </Link>
-                <Link to='/sanctuary'> Settings </Link>
+                <NavLink to='/animals' className={({isActive}) => isActive ? }> Animals </NavLink>
+                <NavLink to='/sponsors'> Sponsors </NavLink>
+                <NavLink to='/volunteers'> Volunteers </NavLink>
+                <NavLink to='/sanctuary'> Settings </NavLink>
             </div>
         </nav>
     )
