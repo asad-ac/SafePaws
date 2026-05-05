a8afae54dd196b9447fb91f52f9b52aa4e2c17bc
asad-ac <asadachau@gmail.com>
2026-05-05 17:21:02 -0400

    feat: finally fixed sanctuary not showing up had to change fetch

--- server/routes/sanctuary.js
+++ server/routes/sanctuary.js
@@ -3,7 +3,7 @@ import sanctuaryController from '../controllers/sanctuary.js'
 
 const router = express.Router()
 
-router.get('/:sanctuary_id', sanctuaryController.getSanctuary)
-router.patch('/:sanctuary_id', sanctuaryController.updateSanctuary)
+router.get('/me', sanctuaryController.getSanctuary)
+router.patch('/me', sanctuaryController.updateSanctuary)
 
 export default router
